b9eccc421ed5bfac637d926f412c49b600a2bd99
Sarath Kumar S <54178445+Sarath-Kumar-S@users.noreply.github.com>
2020-12-06 19:21:24 +0530

    Add files via upload

--- Process Synchronization/Symbol Table Function/symbol.c
+++ Process Synchronization/Symbol Table Function/symbol.c
@@ -1,121 +1,234 @@
 #include<stdio.h>
-#include<stdlib.h>
+#include<conio.h>
 #include<string.h>
-struct table
+#include<stdlib.h>
+int size=0;
+void Insert();
+void Display();
+void Delete();
+int Search(char lab[]);
+void Modify();
+struct SymbTab
 {
-char var[10];
-int value;
-};
-struct table tb1[20];
-int i,j,n;
-void create();
-void modify();
-int search(char variable[],int n);
-void insert();
-void display();
-void main(){
-int ch,result=0;
-char v[10];
-
-do{
-printf("\n1.CREATE\t2.INSERT\t3.MODIFY\t4.SEARCH\t5.DISPLAY\t6.EXIT :");
-scanf("%d",&ch);
-switch(ch)
+ char label[10],symbol[10];
+ int addr;
+struct SymbTab *next;};
+struct SymbTab *first,*last;
+void main()
 {
-case 1:create(); break;
-case 2:insert(); break;
-case 3:modify(); break;
-case 4:printf("\nEnter the variable to be searched:"); scanf("%s",&v);
-result=search(v,n);
-if(result==0)
-printf("\nThe variable is not present\n"); else
-printf("\nThe location of the variable is %d \n The value of %s is %d.",result,tb1[result].var,tb1[result].value);
-break;
-case 5:display(); break;
-case 6:exit(1);
+ int op,y;
+ char la[10];
+ do
+ {
+  printf("\n\tSYMBOL TABLE IMPLEMENTATION");
+  printf("\n\t1.INSERT\t2.DISPLAY\t3.DELETE\t4.SEARCH\t5.MODIFY\t6.END\n");
+  printf("\n\tEnter your option : ");
+  scanf("%d",&op);
+  switch(op)
+  {
+   case 1:
+      Insert();
+      break;
+   case 2:
+      Display();
+      break;
+   case 3:
+      Delete();
+      break;
+   case 4:
+      printf("\n\tEnter the label to be searched : ");
+      scanf("%s",la);
+      y=Search(la);
+      printf("\n\tSearch Result:");
+      if(y==1)
+    printf("\n\tThe label is present in the symbol table\n");
+      else
+    printf("\n\tThe label is not present in the symbol table\n");
+      break;
+   case 5:
+      Modify();
+      break;
+   case 6:
+      exit(0);
+  }
+ }while(op<6);
+ getch();
 }
-}while(ch!=6);
-
-}
-void create(){
-printf("\nEnter the no. of entries:"); scanf("%d",&n);
-printf("\nEnter the variable and the values:-\n");
-for(i=1;i<=n;i++) {
-scanf("%s%d",tb1[i].var,&tb1[i].value);
-check:
-if(tb1[i].var[0]>='0' && tb1[i].var[0]<='9') {
-printf("\nVariable should start with alphabet\nEnter correct name\n");
-scanf("%s%d",tb1[i].var,&tb1[i].value);
-goto check;
-}
-check1:
-for(j=1;j<i;j++) {
-if(strcmp(tb1[i].var,tb1[j].var)==0) {
-printf("\nThe variable already present. Enter another:");
-scanf("%s%d",&tb1[i].var,&tb1[i].value);
-goto check1;
-} } }
-printf("\nThe table after creation is:\n");
-display();
-}
-void insert() {
-if(i>=20)
-printf("\nCannot insert.table is full\n");
-else {
-n++;
-printf("\nEnter the variable and the value:");
-scanf("%s%d",&tb1[n].var,&tb1[n].value); check:
-
-if(tb1[i].var[0]>='0' && tb1[i].var[0]<='9'){
-printf("\nVariable should start with alphabet\nEnter correct name\n");
-scanf("%s%d",tb1[i].var,&tb1[i].value);
-goto check;
-}
-check1:
-for(j=1;j<n;j++){
-if(strcmp(tb1[j].var,tb1[i].var)==0){
-printf("\nThe variable already present. Enter another:");
-scanf("%s%d",&tb1[i].var,&tb1[i].value);
-goto check1;
-}
-}
-printf("\nThe table after insertion is:"); display();
-}
-}
-void modify()
+void Insert()
 {
-char variable[10]; int result=0;
-printf("\nEnter the variable to be modified:");
-scanf("%s",&variable); result=search(variable,n);
-if(result==0)
-printf("%s not present\n",variable);\
-else{
-printf("\nThe current value of the variable %s is %d.\nEnter the new variable and its value",tb1[result].var,tb1[result].value);
-scanf("%s%d",tb1[result].var,&tb1[result].value);
-check:
-if(tb1[i].var[0]>='0' && tb1[i].var[0]<='9'){
-printf("\nVariable should start with alphabet\nEnter correct name\n");
-scanf("%s%d",tb1[i].var,&tb1[i].value);
-goto check;
+  int n;
+  char l[10];
+  printf("\n\tEnter the label : ");
+  scanf("%s",l);
+  n=Search(l);
+  if(n==1)
+   printf("\n\tThe label exists already in the symbol table\n\tDuplicate can't be inserted");
+  else
+   {
+    struct SymbTab *p;
+    p=malloc(sizeof(struct SymbTab));
+    strcpy(p->label,l);
+    printf("\n\tEnter the symbol : ");
+    scanf("%s",p->symbol);
+    printf("\n\tEnter the address : ");
+    scanf("%d",&p->addr);
+    p->next=NULL;
+    if(size==0)
+     {
+      first=p;
+      last=p;
+     }
+    else
+     {
+      last->next=p;
+      last=p;
+     }
+    size++;
+   }
+   printf("\n\tLabel inserted\n");
 }
+void Display()
+{
+  int i;
+  struct SymbTab *p;
+  p=first;
+  printf("\n\tLABEL\t\tSYMBOL\t\tADDRESS\n");
+  for(i=0;i<size;i++)
+   {
+    printf("\t%s\t\t%s\t\t%d\n",p->label,p->symbol,p->addr);
+    p=p->next;
+   }
 }
-printf("\nThe table after modification is:"); display();
+int Search(char lab[])
+{
+ int i,flag=0;
+ struct SymbTab *p;
+ p=first;
+  for(i=0;i<size;i++)
+   {
+    if(strcmp(p->label,lab)==0)
+     flag=1;
+    p=p->next;
+   }
+ return flag;
 }
-
-int search(char variable[],int n){
-int flag; for(i=1;i<=n;i++)
-if(strcmp(tb1[i].var,variable)==0){
-flag=1;
-break;
+void Modify()
+{
+  char l[10],nl[10];
+  int add,choice,i,s;
+  struct SymbTab *p;
+  p=first;
+  printf("\n\tWhat do you want to modify?\n");
+  printf("\n\t1.Only the label\n\t2.Only the address\n\t3.Both the label and address\n");
+  printf("\tEnter your choice : ");
+  scanf("%d",&choice);
+  switch(choice)
+   {
+     case 1:
+       printf("\n\tEnter the old label : ");
+       scanf("%s",l);
+       s=Search(l);
+       if(s==0)
+     printf("\n\tLabel not found\n");
+       else
+    {
+     printf("\n\tEnter the new label : ");
+     scanf("%s",nl);
+     for(i=0;i<size;i++)
+      {
+       if(strcmp(p->label,l)==0)
+         strcpy(p->label,nl);
+       p=p->next;
+      }
+     printf("\n\tAfter Modification:\n");
+     Display();
+    }
+    break;
+    case 2:
+       printf("\n\tEnter the label where the address is to be modified : ");
+       scanf("%s",l);
+       s=Search(l);
+       if(s==0)
+     printf("\n\tLabel not found\n");
+       else
+    {
+     printf("\n\tEnter the new address : ");
+     scanf("%d",&add);
+     for(i=0;i<size;i++)
+      {
+       if(strcmp(p->label,l)==0)
+        p->addr=add;
+       p=p->next;
+      }
+     printf("\n\tAfter Modification:\n");
+     Display();
+    }
+    break;
+    case 3:
+       printf("\n\tEnter the old label : ");
+       scanf("%s",l);
+       s=Search(l);
+       if(s==0)
+     printf("\n\tLabel not found\n");
+       else
+    {
+     printf("\n\tEnter the new label : ");
+     scanf("%s",nl);
+     printf("\n\tEnter the new address : ");
+     scanf("%d",&add);
+     for(i=0;i<size;i++)
+      {
+       if(strcmp(p->label,l)==0)
+        {
+         strcpy(p->label,nl);
+         p->addr=add;
+        }
+       p=p->next;
+      }
+     printf("\n\tAfter Modification:\n");
+     Display();
+    }
+    break;
+    }
 }
-if(flag==1)
-return i;
-else
-return 0;
-}
-
-void display(){
-printf("\nVariable\tvalue\n");
-for(i=1;i<=n;i++)
-printf("%s\t\t%d\n",tb1[i].var,tb1[i].value);
+void Delete()
+{
+  int a;
+  char l[10];
+  struct SymbTab *p,*q;
+  p=first;
+  printf("\n\tEnter the label to be deleted : ");
+  scanf("%s",l);
+  a=Search(l);
+  if(a==0)
+    printf("\n\tLabel not found\n");
+  else
+   {
+    if(strcmp(first->label,l)==0)
+    first=first->next;
+    else if(strcmp(last->label,l)==0)
+     {
+      q=p->next;
+      while(strcmp(q->label,l)!=0)
+      {
+       p=p->next;
+       q=q->next;
+      }
+      p->next=NULL;
+      last=p;
+     }
+    else
+     {
+      q=p->next;
+      while(strcmp(q->label,l)!=0)
+      {
+       p=p->next;
+       q=q->next;
+      }
+      p->next=q->next;
+    }
+    size--;
+    printf("\n\tAfter Deletion:\n");
+    Display();
+   }
 }
